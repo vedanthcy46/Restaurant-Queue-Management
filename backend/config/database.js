3d7f46a7313cfcfc0c7fa1db4a807ba4e374c9e6
Vedanth C Y <vedanth8319@gmail.com>
2026-04-13 21:41:58 +0530

    Refactor database connection to use better-sqlite3
    Removed async DB initialization and switched to better-sqlite3 for synchronous access.

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,9 @@
-const { open } = require('sqlite');
 const Database = require('better-sqlite3');
-const db = new Database('../restaurant.db');
 const path = require('path');
 
+// Create DB instance (sync)
+const db = new Database(path.join(__dirname, '../restaurant.db'));
 
-const initializeDB = async () => {
-    db = await open({
-        filename: path.join(__dirname, '../restaurant.db'),
-        driver: sqlite3.Database
-    });
-    return db;
-};
+console.log('Connected to SQLite (better-sqlite3)');
 
-const getDB = () => db;
-
-module.exports = { initializeDB, db: getDB };
+module.exports = db;
